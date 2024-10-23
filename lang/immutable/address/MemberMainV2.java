3e341da4e8fb6ba2db52c4882b96e12100810702
minho <pmmh9395@gmail.com>
2024-10-23 15:11:09 +0900

    복습 주석추가

--- lang/immutable/address/MemberMainV2.java
+++ lang/immutable/address/MemberMainV2.java
@@ -15,6 +15,7 @@ public class MemberMainV2 {
         //회원B의 주소를 부산으로 변경해야함
         //memberB.getAddress().setValue("부산"); //컴파일 오류
         memberB.setAddress(new ImmutableAddress("부산"));
+        memberB.setAddress(new ImmutableAddress("인천"));
         System.out.println("부산 -> memberB.address");
         System.out.println("memberA = " + memberA);
         System.out.println("memberB = " + memberB);
