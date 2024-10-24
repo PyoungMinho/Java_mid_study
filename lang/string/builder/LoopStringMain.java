cdf3bd4d386c6639934eaa790858c91ce9eda47b
minho <pmmh9395@gmail.com>
2024-10-24 14:17:56 +0900

    String Builder, Chaining done

--- lang/string/builder/LoopStringMain.java
+++ lang/string/builder/LoopStringMain.java
@@ -3,14 +3,17 @@ package lang.string.builder;
 public class LoopStringMain {
 
     public static void main(String[] args) {
+        // 앵간해서는 + 연산자 쓰면 되는데 힘든경우가 있다 밑에 코드로 알아보자
         long startTime = System.currentTimeMillis();
         String result = "";
+
         for (int i = 0; i < 100000; i++) {
             result += "Hello Java ";
         }
+
         long endTime = System.currentTimeMillis();
 
         System.out.println("result = " + result);
-        System.out.println("time = " + (endTime - startTime) + "ms");
+        System.out.println("time = " + (endTime - startTime) + "ms"); //time = 2840ms but StringBuilder 사용하면 time = 6ms
     }
 }
--- lang/string/builder/StringBuilderMain1_1.java
+++ lang/string/builder/StringBuilderMain1_1.java
@@ -10,17 +10,25 @@ public class StringBuilderMain1_1 {
         sb.append("D");
         System.out.println("sb = " + sb);
 
-        sb.insert(4, "Java");
+        String str = "A" + "B" + "C" + "D";
+        System.out.println("str = " + str); //이렇게 해도 되지만 StringBuilder 를 이용하는 이유
+                                            // String str = String("A") + String("B") + String("C") + String("D");
+                                            // String str = new String("AB")+ String("C") + String("D");
+                                            // String str = new String("ABC")+ String("D");
+                                            // String str = new String("ABCD"); 흐름으로 진행되기 때문에 효율차이
+                                            // But 자바가 간단한 리터럴 + 리터럴은 최적화로 String상수풀에 넣어서 사용해서 짧은거 더하면 그냥 + 해도됌
+
+        sb.insert(4, " Java");
         System.out.println("insert = " + sb);
 
-        sb.delete(4, 8);
+        sb.delete(4, 8); // "Java" 의 a 가 남아서 끝에 달라붙는다.
         System.out.println("delete = " + sb);
 
         sb.reverse();
         System.out.println("reverse = " + sb);
 
         //StringBuilder -> String
-        String string = sb.toString();
+        String string = sb.toString(); // 다시 불변으로 변경
         System.out.println("string = " + string);
     }
 }
