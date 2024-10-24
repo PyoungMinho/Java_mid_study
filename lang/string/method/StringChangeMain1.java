4dc2537dc46882dfe9b7da20a0cc01bd71068493
minho <pmmh9395@gmail.com>
2024-10-24 13:18:11 +0900

    String method done

--- lang/string/method/StringChangeMain1.java
+++ lang/string/method/StringChangeMain1.java
@@ -5,6 +5,7 @@ public class StringChangeMain1 {
     public static void main(String[] args) {
         String str = "Hello, Java! Welcome to Java";
 
+        System.out.println("인덱스 7부터의 부분 문자열: " + str.substring(6));
         System.out.println("인덱스 7부터의 부분 문자열: " + str.substring(7));
         System.out.println("인덱스 7부터 12까지의 부분 문자열: " + str.substring(7, 12));
 
@@ -12,5 +13,7 @@ public class StringChangeMain1 {
 
         System.out.println("'Java'를 'World'로 대체: " + str.replace("Java", "World"));
         System.out.println("첫 번째 'Java'를 'World'로 대체: " + str.replaceFirst("Java", "World"));
+
+        System.out.println("Immutable String : "+str); // String은 불변이니깐 바꾸고 싶으면 새로운 참조로 받아야함
     }
 }
